e98f716412087bf46972ddddb3052d8cc744c663
Anitha Boppagowdanapura Shivalingaswamy <anithabs.ec@gmail.com>
2017-08-10 09:25:39 -0500

    Fragment

--- app/src/main/java/com/project/uwm/mydiabitiestracker/RecordFragment/FoodGraphFragment.java
+++ app/src/main/java/com/project/uwm/mydiabitiestracker/RecordFragment/FoodGraphFragment.java
@@ -8,64 +8,43 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.Entry;
 import com.project.uwm.mydiabitiestracker.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FoodGraphFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FoodGraphFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class FoodGraphFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+
+public class FoodGraphFragment extends Fragment {
+
 
     private OnFragmentInteractionListener mListener;
+    BarChart foodBarChart;
 
     public FoodGraphFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FoodGraphFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FoodGraphFragment newInstance(String param1, String param2) {
-        FoodGraphFragment fragment = new FoodGraphFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food_graph, container, false);
+
+        View  rootView =inflater.inflate(R.layout.fragment_food_graph, container, false);
+        foodBarChart= (BarChart) getActivity().findViewById(R.id.foodBarChart);
+
+        ArrayList<Entry> xAxisTypeOfFood = new ArrayList<>();
+        ArrayList<Entry> yAxisAmountOfFood = new ArrayList<>();
+
+        return rootView;
+
     }
 
     // TODO: Rename method, update argument and hook method into UI event
